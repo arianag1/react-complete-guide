2891bc89fc3e5b6dbc9d54acdfc828f23ea0271a
Ariana.Gardner <ariana.gardner@americanwell.com>
2022-12-09 14:29:02 -0500

    Update Expenses.js

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,35 +3,30 @@ import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div>
-      <div className="expenses">
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-      </div>
-      <div className="expenses">
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-      </div>
-      <div className="expenses">
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-      </div>
-      <div className="expenses">
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
-      </div>
+    <div className="expenses">
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+
+      <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      />
+
+      <ExpenseItem
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      />
     </div>
   );
 }
